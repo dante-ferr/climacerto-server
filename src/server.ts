c77c6c39b409a99b2e6758798413272a8939cb86
Rodrigo Jeronimo <86856395+TheRodrig0@users.noreply.github.com>
2025-10-06 03:31:09 +0000

    hot fix

--- src/server.ts
+++ src/server.ts
@@ -14,10 +14,12 @@ dns.setDefaultResultOrder('ipv4first')
 async function buildApp() {
   const app = Fastify({ logger: true })
 
-  await app.register(helmet)
   await app.register(cors, {
-    origin: "https://climacerto.onrender.com/"
+    origin: "https://climacerto.onrender.com"
   })
+
+  await app.register(helmet)
+
   await app.register(rateLimit, {
     max: 100,
     timeWindow: "1 minute",
