b22aa5fef5da7399da9889127a31469bfc6e2f4a
johanesgauss <226770400+Johannesgauss@users.noreply.github.com>
2025-10-06 09:49:46 -0300

    commit que o Rodrigo pediu

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import type { FastifyInstance } from 'fastify'
 import { analyzeContainer } from './modules/analyze/assets/analyze-container.js'
 import { registerControllers } from './modules/shared/decorators/routes.js'
 import { errorHandler } from './modules/shared/middlewares/error-middleware.js'
-import cors from "@fastify/cors"
 import helmet from "@fastify/helmet"
 import rateLimit from "@fastify/rate-limit"
 
@@ -14,16 +13,39 @@ dns.setDefaultResultOrder('ipv4first')
 async function buildApp() {
   const app = Fastify({ logger: true })
 
-  await app.register(cors, {
-    origin: "https://climacerto.onrender.com"
-  })
-
   await app.register(helmet)
 
   await app.register(rateLimit, {
     max: 100,
     timeWindow: "1 minute",
   })
+
+  // Configuração CORS manual
+  app.addHook('preHandler', (request, reply, done) => {
+    // Defina as origens permitidas
+    const allowedOrigins = [
+      "https://climacerto.onrender.com/",
+      "https://climacerto-server-no3v5q7wc-therodrig0s-projects.vercel.app/"
+    ];
+    const origin = request.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+      reply.header('Access-Control-Allow-Origin', origin);
+    }
+
+    // Defina os métodos permitidos (apenas GET)
+    reply.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    // Responda imediatamente a requisições pre-flight (OPTIONS)
+    if (request.method === 'OPTIONS') {
+      reply.statusCode = 200;
+      reply.send();
+      return;
+    }
+
+    done();
+  });
 
   app.setErrorHandler(errorHandler)
   registerControllers(app, "api", analyzeContainer)
